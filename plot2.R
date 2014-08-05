aa3652799a48b201081b5b644b4bbf2893fee76a
Jon Sundwall <jon.sundwall@cgi.com>
2014-08-05 22:44:23 +0300

    plot2 done

---
+++ plot2.R
@@ -0,0 +1,28 @@
+
+plot2 <- function(fname) {
+    # read the whole data file
+    alldata <- read.csv(
+        fname, sep = ";", 
+        dec = ".", 
+        na.strings = "?"
+    )
+    # combine date and time columns to a timestamp and select subset by date range
+    alldata <- transform(alldata, Timestamp = strptime(paste(Date, Time), format="%d/%m/%Y %H:%M:%S"))
+    startDate <- as.POSIXlt("2007-02-01")
+    endDate <- as.POSIXlt("2007-02-03")
+    data <- subset(alldata, Timestamp >=startDate & Timestamp < endDate)
+    
+    # open the target file
+    png(filename = "plot2.png", width = 480, height = 480)
+    
+    # create the plot
+    plot(data$Timestamp, data$Global_active_power, 
+         ylab = "Global Active Power (kilowatts)",
+         xlab="",
+         type="l")
+    
+    # close the file
+    dev.off()
+    
+    data
+}
